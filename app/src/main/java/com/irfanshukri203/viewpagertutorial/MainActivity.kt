50a6365f8ebe680755a383b7ff04d97cadbc58ae
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-21 22:18:04 +0800

    two images can swipe vertically using ViewPager2

--- app/src/main/java/com/irfanshukri203/viewpagertutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/viewpagertutorial/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.irfanshukri203.viewpagertutorial
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val images = listOf(R.drawable.cat1, R.drawable.cat2)
+        val viewImageAdapter = ViewPagerAdapter(images)
+
+        viewPager.adapter = viewImageAdapter
+
+        viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+
+        viewPager.fakeDragBy(-10F)
+        viewPager.endFakeDrag()
     }
 }
---
+++ app/src/main/java/com/irfanshukri203/viewpagertutorial/ViewPagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.irfanshukri203.viewpagertutorial
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_view_pager.view.*
+
+class ViewPagerAdapter(val images: List<Int>) : RecyclerView.Adapter<ViewPagerAdapter.ViewPagerViewHolder>() {
+    inner class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view_pager, parent, false)
+
+        return ViewPagerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
+        val currentImage = images[position]
+        holder.itemView.ivDummyImage.setImageResource(currentImage)
+    }
+
+    override fun getItemCount(): Int = images.size
+}
